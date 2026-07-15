ac3baae0ce58a58e032da4e2b4905df5f20af15f
maiconx4-hue <maicon.x4@gmail.com>
2026-07-15 09:28:05 -0300

    Atualizar o site.py
    criando o site.py para mostrar as UBS e detalhes e mapa

--- routes/site.py
+++ routes/site.py
@@ -2,14 +2,18 @@ from flask import Blueprint, render_template
 
 from models import UBS, Estoque
 
-
 site_bp = Blueprint("site", __name__)
 
 
 @site_bp.route("/")
 def index():
+
     ubs = UBS.query.all()
-    return render_template("index.html", ubs=ubs)
+
+    return render_template(
+        "index.html",
+        ubs=ubs
+    )
 
 
 @site_bp.route("/ubs/<int:ubs_id>")
@@ -17,7 +21,14 @@ def detalhes_ubs(ubs_id):
 
     ubs = UBS.query.get_or_404(ubs_id)
 
+    estoques = (
+        Estoque.query
+        .filter_by(ubs_id=ubs.id)
+        .all()
+    )
+
     return render_template(
         "ubs_detalhes.html",
-        ubs=ubs
+        ubs=ubs,
+        estoques=estoques
     )
