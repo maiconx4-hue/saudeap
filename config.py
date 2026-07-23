973552114f5b3c76dfcfab11dc18580b28687504
maiconx4-hue <maicon.x4@exemplo.com>
2026-07-23 20:01:41 -0300

    Corrige configuração do Flask-Mail

--- config.py
+++ config.py
@@ -69,16 +69,24 @@ class Config:
 
     #URL_SISTEMA = "http://localhost:5000"
     URL_SISTEMA = "https://saudeap.onrender.com"
+    
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
 
-    MAIL_SERVER = os.getenv("MAIL_SERVER")
-    MAIL_PORT = int(os.getenv("MAIL_PORT"))
+    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
 
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
+
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
+
+    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "False") == "True"
 
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 
-    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
+    MAIL_DEFAULT_SENDER = os.getenv(
+        "MAIL_DEFAULT_SENDER",
+        MAIL_USERNAME
+    )
 
 
